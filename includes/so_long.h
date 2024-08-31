99e316c4a1913104ceab44e011a690b1af587846
bepoisso <poisson.benjamin69@gmail.com>
2024-08-31 18:09:03 +0200

    try to understand minilibx in progress

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,16 +6,35 @@
 /*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 16:10:42 by bepoisso          #+#    #+#             */
-/*   Updated: 2024/08/31 16:25:24 by bepoisso         ###   ########.fr       */
+/*   Updated: 2024/08/31 18:07:57 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+//_________________INCLUDES_________________
 # include "mlx.h"
 # include "../libft/includes/libft.h"
 # include "../libft/includes/ft_printf.h"
 # include <unistd.h>
+
+//_________________STRUCT_________________
+typedef struct s_mlx_data
+{
+	void	*link;
+	void	*screen;
+}	t_mlx_data;
+
+enum
+{
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
 
 #endif
--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,26 +6,17 @@
 /*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 16:01:39 by bepoisso          #+#    #+#             */
-/*   Updated: 2024/08/31 17:09:48 by bepoisso         ###   ########.fr       */
+/*   Updated: 2024/08/31 18:08:20 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int	deal_key(int key, void *param)
-{
-	write(1, "PRESS\n", 6);
-	return (0);
-}
-
 int	main(void)
 {
-	void	*ptr_mlx;
-	void	*ptr_window;
+	t_mlx_data	data;
 
-	ptr_mlx = mlx_init();
-	ptr_window = mlx_new_window(ptr_mlx, 500, 500, "So_long");
-	mlx_pixel_put(ptr_mlx, ptr_window, 250, 250, 255 + 255 + 255);
-	mlx_key_hook(ptr_window, &deal_key, NULL);
-	mlx_loop(ptr_mlx);
+	data.link = mlx_init();
+	data.screen = mlx_new_window(data.link, 500, 500, "So_long");
+	mlx_loop(data.link);
 }
