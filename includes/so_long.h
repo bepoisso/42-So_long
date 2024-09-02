861625cbdeb6e4aaf843222e57c13aa811ef009f
Benjamin <poisson.benjamin69@gmail.com>
2024-09-02 15:42:15 +0200

    update

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 16:10:42 by bepoisso          #+#    #+#             */
-/*   Updated: 2024/08/31 18:07:57 by bepoisso         ###   ########.fr       */
+/*   Updated: 2024/09/02 15:36:55 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define SO_LONG_H
 
 //_________________INCLUDES_________________
-# include "mlx.h"
+# include "../minilibx-linux/mlx.h"
 # include "../libft/includes/libft.h"
 # include "../libft/includes/ft_printf.h"
 # include <unistd.h>
--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,11 +6,17 @@
 /*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 16:01:39 by bepoisso          #+#    #+#             */
-/*   Updated: 2024/08/31 18:08:20 by bepoisso         ###   ########.fr       */
+/*   Updated: 2024/09/02 15:41:46 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+int	ft_void(void)
+{
+	ft_putchar_fd('X', 1);
+	return (1);
+}
 
 int	main(void)
 {
@@ -18,5 +24,6 @@ int	main(void)
 
 	data.link = mlx_init();
 	data.screen = mlx_new_window(data.link, 500, 500, "So_long");
+	mlx_key_hook(data.screen, &ft_void, (void *)NULL);
 	mlx_loop(data.link);
 }
