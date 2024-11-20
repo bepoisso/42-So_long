72d88bb0d183a24b9e2b235ad91c248884ad9d54
bepoisso <bepoisso@student.42perpignan.fr>
2024-11-20 19:12:34 +0100

    WIP : scoreboard

--- srcs/scoreboard.c
+++ srcs/scoreboard.c
@@ -6,13 +6,11 @@
 /*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 11:20:47 by bepoisso          #+#    #+#             */
-/*   Updated: 2024/11/19 17:18:36 by bepoisso         ###   ########.fr       */
+/*   Updated: 2024/11/20 19:11:18 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
-
-//PLAYER\nSCORE\n
 
 void	save_in_scoreboard(t_mlx_data *data)
 {
@@ -22,16 +20,16 @@ void	save_in_scoreboard(t_mlx_data *data)
 
 	split_score("./srcs/pacman.sb", score, user);
 	if (get_index_of_user(user, data->pseudo, 0) != -1)
+	{
 		index = get_index_of_user(user, data->pseudo, 0);
 		if (comp_score(score, data->map.move_count, index))
 			score[index] = data->map.move_count;
-		else
-		{
-			print_new_scoreboard(score, user, "./srcs/pacman.sb")
-			add_score();
-		}
-	sort_score();
-	aff_scoreboard();
+	}
+	else
+		add_score(data->pseudo, data->map.move_count, "./srcs/pacma.sb");
+	sort_score(&user, &score);
+	print_new_score(&user, &score);
+	aff_scoreboard("./srcs/pacman.sb");
 
 }
 
@@ -78,42 +76,84 @@ int	comp_score(char **score, int new_score, int index)
 	return (0);
 }
 
-void	print_new_scoreboard(char **score, char **user, char *file)
+void	print_new_score(char **user, char **score)
 {
-	int	i;
 	int	fd;
+	int	i;
 
-	fd = open(file, O_APPEND | O_WRONLY);
-	if (!fd)
-		return (ft_printf("***ERROR OPEN PRINT_NEW_SCOREBOARD***"), NULL);
 	i = 0;
+	fd = open("./srcs/temp.sb", O_CREAT | O_APPEND | O_WRONLY);
+	if (fd < 0)
+		return ((void)ft_printf("***ERROR OPEN PRIT_NEW_SCORE***"));
+
 	while(user[i])
 	{
-		ft_putendl_fd(user[i], fd);
-		ft_putnbr_fd(score[i], fd);
-		ft_putchar_fd('\n', fd);
+		add_score(user[i], score[i], "./srcs/temp.sb");
+		i++;
 	}
+	unlink("./srcs/pacman.sb");
+	link("./srcs/temp.sb", "./srcs/pacman.sb");
+	unlink("./srcs/temp.sb");
+	close(fd);
 }
 
-void	add_score(char *player_name, int player_score, char *file)
+void	add_score(char *player_name, char *player_score, char *file)
 {
 	int	fd;
 
 	fd = open(file, O_APPEND | O_WRONLY);
 	if (!fd)
 		return (ft_printf("***ERROR OPEN ADD_SCORE***"), NULL);
+
 	ft_putendl_fd(player_name, fd);
-	ft_putnbr_fd(player_score, fd);
-	ft_putchar_fd('\n', fd);
+	ft_putendl_fd(player_score, fd);
+
+	clode (fd);
 }
 
-void	sort_score()
+void	sort_score(char **user, char **score)
 {
+	char	*temp_score;
+	char	*temp_user;
+	int		i;
+	int		j;
 
+	j = 0;
+	while(score[j + 1])
+	{
+		i = 0;
+		while (score[i + 1])
+		{
+			if (ft_atoi(score[i]) > ft_atoi(score[i + 1]))
+			{
+				temp_score = score[i];
+				temp_user = user[i];
+				score[i] = score[i + 1];
+				user[i] = user[i + 1];
+				score[i + 1] = temp_score;
+				user[i + 1] = temp_user;
+			}
+			i++;
+		}
+		j++;
+	}
 }
 
 
-void	aff_scoreboard()
+void	aff_scoreboard(char *file)
 {
+	int		fd;
+	char	*str;
 
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return ((void)ft_printf("***ERROR OPEN FILE AFF_SCOREBOARD***"));
+
+	str = get_next_line(fd);
+	while (str)
+	{
+		ft_printf("%s", str);
+		str = get_next_line(fd);
+	}
+	clode(fd);
 }
